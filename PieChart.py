9481006757b834af171e10384729c1e72f3a1e3e
Pavle Ivanović <105980398+pavleiv@users.noreply.github.com>
2024-07-21 23:11:30 +0200

    Malo sređivanje koda.

--- PieChart.py
+++ PieChart.py
@@ -1,18 +1,19 @@
-import json
 import io
+import json
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+from AIModel import *
 from prompts import *
 
 class PieChart:
-    def __init__(self, model):
+    def __init__(self, model: AIModel):
         # Constructor
         self.model = model
 
     def fig_to_img(self, fig: plt.Figure) -> Image.Image:
-        # Converts a Matplotlib figure to a PIL Image.
+        # Converts a Matplotlib figure to a PIL Image
         buf = io.BytesIO()
         fig.savefig(buf, format='png')
         buf.seek(0)
@@ -21,18 +22,23 @@ class PieChart:
     
     def crop_data(self, data: str) -> str:
         # Crops model's response from first exclamation mark to last
-        first_exclamation_index = data.find('!')
-        last_exclamation_index = data.rfind('!')
-            
-        if first_exclamation_index != -1 and first_exclamation_index != last_exclamation_index:
-            return data[first_exclamation_index + 1:last_exclamation_index]
-        elif first_exclamation_index != -1:
-            return data[first_exclamation_index + 1:]
-        return data
+        first_excl = data.find('!')
+        last_excl = data.rfind('!')
+        
+        if first_excl == -1 and last_excl == -1:
+            return data
+        
+        if first_excl == (len(data) - 2):
+            return data[:last_excl]
+        
+        if last_excl == 0:
+            return data[first_excl + 1:]
+        
+        return data[first_excl + 1:last_excl]
     
 
     def create_pie_chart(self, data: dict) -> plt.Figure:
-        # Creates a pie chart from the provided data.
+        # Creates a pie chart from the provided data
         sorted_data = sorted(data.values(), key=lambda x: x['Value'], reverse=False)
 
         labels = [info['Label'] for info in sorted_data]
@@ -51,9 +57,9 @@ class PieChart:
         ax.axis('equal')
         return fig
 
-    def give_chart(self, imggg: np.array) -> Image.Image:
-        # Processes an image, extracts pie chart data, and generates a pie chart.
-        answer = self.model.get_response(pie_chart_prompt, imggg)
+    def give_chart(self, img: np.array) -> Image.Image:
+        # Processes an image, extracts pie chart data, and generates a pie chart
+        answer = self.model.get_response(pie_chart_prompt, img)
         answer = self.crop_data(answer)
         
         data = json.loads(answer)
@@ -61,7 +67,3 @@ class PieChart:
         img_format = self.fig_to_img(fig)
         
         return img_format
-
-    def get_response(self, message: str, image: np.array) -> str:
-        # Sends an image and a message to the model and returns the response. class.
-        return self.model.get_response(message, image)
