98b133e1149064a2fd52d5682932d96f33d81d32
Pavle Ivanović <105980398+pavleiv@users.noreply.github.com>
2024-07-27 21:11:30 +0200

    Dodavanje promptova vezano za bar i line chart. Sređivanje

--- prompts.py
+++ prompts.py
@@ -1,13 +1,23 @@
 orchestrator_prompt = """
-You are an AI assistant made for information analysis.
-Your main goal is to recognize is the given photo a: pie chart, bar chart, line chart, or none of these.
-Let's think step by step.
-Step 1: Look at the image provided.
-Step 2: Think about what is a pie chart, what is a bar chart, what is a line chart before you respond.
-Step 3: Identify the type of chart in the image.
-Step 4: Check if what you think is correct.
-Step 5: Respond with "pie chart" if it is a pie chart, "bar chart" if it is a bar chart, "line chart" if it is a line chart, "trash" if it is anything else.
+You are an AI assistant made for information analysis. Your main goal is to recognize if the given photo is a pie chart, bar chart, line chart, or none of these.
+
+Let's think step by step:
+Step 1: Carefully examine the image provided.
+Step 2: Recall the defining characteristics of a pie chart, bar chart, and line chart:
+A pie chart is circular and divided into slices to illustrate numerical proportions.
+A bar chart displays rectangular bars with lengths proportional to the values they represent.
+A line chart displays information as a series of data points called 'markers' connected by straight line segments.
+Step 3: Identify the type of chart in the image based on the characteristics recalled.
+Step 4: Verify your identification by cross-referencing the characteristics.
+Step 5: Respond with one of the following:
+
+"pie chart" if it is a pie chart
+"bar chart" if it is a bar chart
+"line chart" if it is a line chart
+"trash" if it does not fit any of the above categories
 """
+
+
 
 pie_chart_prompt =  """
 You are an AI assistant and you will be given a pie chart.
@@ -18,12 +28,16 @@ To understand a pie chart, follow these rules:
 1. First, find out how many different parts this pie chart contains. This should be a number.
 2. Then, verify that the number you got is correct.
 3. Next, find out the values, labels, and colors for each part.
+Do not round numbers, if value is 47, do not return 46.5 or something like that.
+If value is 4.9, do not return 5 or  something similar, just 4.9.
 4. Provide information about each part in JSON format with the following keys for each part: Value, Label, Color.
 5. Check provided information TWICE, if there is any error, fix it and check again.
-6. Put ! at the beginning and at the end of JSON format.
+6. Put "&" at the beginning and at the end of JSON format as a delimiter. 
+At the end there should not be any symbols before the delimiter character.
+
 
 This is an example of correctly formatted JSON output:
-!{
+&{
 "Label1": {
 "Value": val1, 
 "Label": lab1, 
@@ -40,21 +54,28 @@ This is an example of correctly formatted JSON output:
 "Label": labN, 
 "Color": colN
 }
-}!
+}&
 This example is not related to your case; it is just an example of the format.
 
 Colors MUST be one of the following(the one that is most similar): 
-'chocolate', 'coral', 'crimson', 'fuchsia', 'gold', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'maroon', 'navy', 'olive', 'orange', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'tan', 'teal']
+aqua, aquamarine, azure, beige, black, blue, brown, chartreuse, chocolate, coral, crimson, 
+cyan, darkblue, darkgreen, fuchsia, gold, goldenrod, green, grey, indigo, ivory, khaki, lavender, 
+lightblue, lightgreen, lime, magenta, maroon, navy, olive, orange, orangered, orchid, pink, plum, 
+purple, red, salmon, sienna, silver, tan, teal, tomato, turquoise, violet, wheat, white, yellow, yellowgreen.
+
 Choose one that is most similar.
 For example, navy blue is similar to blue, but blue is not similar to red.
-
+Blue is not similar to green, red is not similar to blue or green, but red is similar to pink
 While generating the response, follow these rules:
 1) Be careful when you read values and labels.
 2) Double-check to ensure the data you found is accurate.
 3) Answer with JUST ONE JSON format.
 4) Return JUST one JSON without any additional chars or words. 
 5) Don't pay attention to watermarks, words in background etc., act like they don't exist.
+6) Do not show source or any additional info about graph
 """
+
+
 
 line_chart_prompt = """
 You are an AI assistant made for information extraction and data analysis. Your task is to extract the data points from a line chart image and convert them into a JSON format.
@@ -63,16 +84,17 @@ Let's proceed step by step:
 Step 1: Carefully examine the provided line chart image.
 Step 2: Find out the Y axis label.
 Step 3: Check if what you got is true.
-Step 4: Find out how many X values there are on the bottom of the line chart. This should be a number. That is the number of points.
-Step 5: Verify that the number you got is correct.
-Step 6: Figure out if the Y values of every point are next to the point.
-Step 7: If the Y values of every point are next to the point, find out the X and Y values for each point in order. If they are not, for every X value find out what Y value best corresponds to it using the chart.
-Step 8: Provide information about the Y axis and every point in JSON format, as a string.
-Step 9: Check provided information if it is correct.
-Step 10: Put ! at the beginning and at the end of JSON format.
+Step 4: Now pay close attention to the X values on the bottom of the line chart.
+Step 5: Find out how many X values there are on the bottom of the line chart. This should be a number. That is the number of points.
+Step 6: Verify that the number you got is correct.
+Step 7: Figure out if the Y values of every point are next to the point.
+Step 8: If the Y values of every point are next to the point, find out the X and Y values for each point in order. If they are not, for every X value find out what Y value best corresponds to it using the chart. Y values may be a percentage.
+Step 9: With that in mind, provide information about the Y axis and every point in JSON format, as a string. Try to explicitly copy what is said on the graph.
+Step 10: For every point, check if the values are correct.
+Step 11: Put "&" at the beginning and at the end of JSON format as a delimiter. At the end there should not be any symbols before the delimiter character.
 
 This is an example of correctly formatted JSON output:
-!{
+&{
 "Y_Axis": {
 "Label": "lab"
 },
@@ -88,8 +110,8 @@ This is an example of correctly formatted JSON output:
 "PointN": {
 "X_Value": "x_valN", 
 "Y_Value": "y_valN"
-"}
-}!
+}
+}&
 
 This example is not related to your case; it is just an example of the format.
 
@@ -97,12 +119,20 @@ While generating the response, follow these rules:
 1) Be careful when you read values and labels.
 2) Double-check to ensure the data you found is accurate.
 3) Answer with JUST ONE JSON format.
-4) Return JUST one JSON without any additional chars or words. 
-5) Don't pay attention to watermarks, words in background etc., act like they don't exist.
+4) Do not put any more characters at the end of your answer.
+5) Do not under any circumstance show the source of the picture.
 """
 
+line_chart_percentages_prompt = """
+You are an AI assistant made for information extraction and data analysis. Your task is to tell me if the Y values on this line chart are represented by percentages.
+
+Respond with &YES& if there are percentages next to the numbers on the Y axis, and &NO& if there are not. & is a delimiter character. Do not respond with anything else.
+        """
+
+
+
 bar_chart_prompt = """
-        You are an AI assistant and you will be given a bar chart. 
+You are an AI assistant and you will be given a bar chart. 
 Your task is to understand the bar chart. Since this bar chart is very important, you need to focus on it.
 
 Pay attention to orientation of bars, are they horizontal or vertical. 
@@ -138,9 +168,35 @@ While generating the response, follow these rules:
 """
 
 prompt_label = """
-        You are an AI assistant and you will be given a bar chart. 
+You are an AI assistant and you will be given a bar chart. 
+Your task is to understand the bar chart. Since this bar chart is very important, you need to focus on it.
+Pay attention, exist ther  e exact values on chart area or not? Respond with &YES& if there are,
+or &NO& if there are not, based on given chart.
+"""
+
+prompt_xtitle="""
+Respond with what X axis title is. Respond without additional word 
+like X axis title etc. 
+Focus on  region under x axis, because X axis title is ALWAYS written right under the x axis!!
+Do not return any Y axis labels, title is different from them.
+Example of WRONG respond is "X axis title: Manufacturer sales in million GBP", instead "Manufacturer sales in million GBP".
+"""
+
+prompt_ytitle="""
+If chart is vertical, respond with what Y axis title is.Respond without additional word like Y axis title etc. 
+Example of WRONG respind is "Y axis title: Manufacturer sales in million GBP", instead "Manufacturer sales in million GBP".
+"""
+
+prompt_orient="""
+You are an AI assistant and you will be given a bar chart. 
 Your task is to understand the bar chart. Since this bar chart is very important, you need to focus on it.
 
-Pay attention, exist there exact values on chart area or not? Respond with &YES& if there are,
-or &NO& if there are not, based on given chart. 
+Pay attention to orientation of bars, are they horizontal or vertical. 
+Respond with "-Vertical-" or "-Horizontal-" based on given chart.
+"""
+
+prompt_perc = """
+You are an AI assistant and you will be given a bar chart. 
+Pay attention, is chart in percentage or no? Respond with &Y& if it is,
+or &N& if it is not, based on given chart.
 """
